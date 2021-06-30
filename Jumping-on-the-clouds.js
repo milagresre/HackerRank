be69439c1c73d7180776deaa08dd9681b1bcae26
Renata Milagres <renata.milagres@gmail.com>
2021-06-30 18:54:48 -0300

    Rename warmups

--- Jumping-on-the-clouds.js
+++ Jumping-on-the-clouds.js
@@ -7,35 +7,7 @@ for (let i = 0; i < c.length; i++){
     console.log(`c[${i}]: ${c[i]}`)
     console.log(`c[${i+1}]: ${c[i+1]}`)
     console.log(`total: ${totalSteps}`)
-    if (i===0){
-        totalSteps++;
-        console.log(`i: ${i}`)
-        continue;
-    }
-    else {
-        if (c[i] === 0 && c[i+1] === 1){
-            totalSteps++
-            console.log(`total: ${totalSteps}`)
-            continue;
-        }
-        else {
-            if (c[i]===1) {
-                continue;
-            }
-            if(c[i+1]===0) {
-                console.log(`total: ${totalSteps}`)
-                totalSteps++
-                i++
-                continue;
-
-            }
-            console.log(`total: ${totalSteps}`)
-            totalSteps++
-
-        }
-
-
-    }
+   
 
 
 }
